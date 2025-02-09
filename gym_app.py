d04fd15d5886ea886022d92bca3debead70cfa7b
Mateo Mojica <125775402+HaroldMate1@users.noreply.github.com>
2025-02-09 13:39:56 +0100

    Update gym_app.py

--- gym_app.py
+++ gym_app.py
@@ -9,9 +9,6 @@ Original file is located at
 
 !pip install streamlit pandas pyngrok
 
-#Authentication Token
-!ngrok authtoken 2sm6tqzvGnEsD1VGT6GrcPRcXEu_2JncQ9aGdPn1NmG7miY81
-
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile app.py
 # import streamlit as st
